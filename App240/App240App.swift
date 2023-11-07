25631ad171030f4e5a2068af0ff855c05dbd10e2
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2023-11-07 18:07:05 +0300

    Initial Commit

---
+++ App240/App240App.swift
@@ -0,0 +1,17 @@
+//
+//  App240App.swift
+//  App240
+//
+//  Created by IGOR on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct App240App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
